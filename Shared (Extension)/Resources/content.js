86656686919c010a4ab053caa72ad156d01116fe
Philipp Bolte <philipp.bukkit@gmail.com>
2022-08-16 22:11:30 +0200

    feat: add ffz support
    The extension now injects the FrankerFaceZ script as well + some image updates

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,14 @@
 // Inject BBTV Script
-const script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://cdn.betterttv.net/betterttv.js';
-const head = document.getElementsByTagName('head')[0];
-head.appendChild(script);
+const bttvScript = document.createElement('script');
+bttvScript.type = 'text/javascript';
+bttvScript.src = 'https://cdn.betterttv.net/betterttv.js';
 
+// Inject FFZ script
+const ffzScript = document.createElement('script');
+ffzScript.type = 'text/javascript';
+ffzScript.src = 'https://cdn.frankerfacez.com/static/ffz_injector.user.js';
+
+// Append scripts to head
+const head = document.getElementsByTagName('head')[0];
+head.appendChild(bttvScript);
+head.appendChild(ffzScript);
